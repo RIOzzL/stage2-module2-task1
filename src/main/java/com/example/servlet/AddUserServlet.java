ec32342ecf0d81903aba35823549e2d56e3b4eca
Roman Gulevich <r.i.gulevich@gmail.com>
2023-01-26 13:40:43 +0300

    Implemented task

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,29 @@
 package com.example.servlet;
 
-public class AddUserServlet {
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "addUser", urlPatterns = "/add")
+public class AddUserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getServletContext().getRequestDispatcher("/jsp/add.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = new User(req.getParameter("firstName"), req.getParameter("lastName"));
+        Warehouse.getInstance().addUser(user);
+        req.setAttribute("user", user);
+        req.getServletContext().getRequestDispatcher("/jsp/add.jsp").forward(req, resp);
+    }
     //write your code here!
 }
