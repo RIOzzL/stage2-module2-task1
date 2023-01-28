1e50fd40b28004708e69af49ec67584658b8f984
Roman Gulevich <r.i.gulevich@gmail.com>
2023-01-28 20:03:57 +0300

    Fixed AddUserServlet

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "addUser", urlPatterns = "/add")
+@WebServlet(urlPatterns = "/add")
 public class AddUserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
